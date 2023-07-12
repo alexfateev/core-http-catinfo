273693e49d60c13e17adc2cc703c57a850b2e245
Alexandr <mailboxfav@gmail.com>
2023-07-12 21:20:21 +0500

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.IOException;
+import java.util.List;
+
+public class Main {
+
+    public static ObjectMapper mapper = new ObjectMapper();
+
+    public static void main(String[] args) {
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000)    // максимальное время ожидание подключения к серверу
+                        .setSocketTimeout(30000)    // максимальное время ожидания получения данных
+                        .setRedirectsEnabled(false) // возможность следовать редиректу в ответе
+                        .build())
+                .build();
+
+        HttpGet request = new HttpGet("https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats");
+        try {
+            CloseableHttpResponse response = httpClient.execute(request);
+            List<CatTips> list = mapper.readValue(response.getEntity().getContent(), new TypeReference<List<CatTips>>() {
+            });
+            list.stream().filter(value -> value.getUpvotes() > 0).forEach(System.out::println);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
